c01452506b177581f9797e28f956b59eaf96a7c2
Steford032 <prudniko0356@gmail.com>
2023-05-12 23:30:32 +0300

    First commit

---
+++ Client/Client/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+
+        static async Task Main(string[] args)
+        {
+            using Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                await client.ConnectAsync("127.0.0.1", 8888);
+                Console.WriteLine($"Подключение к {client.RemoteEndPoint} успешно установлено");
+            }
+            catch(SocketException exc)
+            {
+                Console.WriteLine($"Не удалось подключиться к {client.RemoteEndPoint} ");
+                Console.WriteLine(exc);
+            }
+
+
+
+        }
+    }
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            IPAddress localIp = new IPAddress(new byte[] { 127, 0, 0, 1 });
+            IPEndPoint point = new IPEndPoint(new IPAddress(new byte[] {127,0,0,1}), 8888);
+            using Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            socket.Bind(point);
+
+            Console.WriteLine($"Сокет был создан (SocketInfo: {socket.LocalEndPoint})");
+            socket.Listen();
+            Console.WriteLine("Ожидание входящих подключений...");
+            using Socket client = await socket.AcceptAsync();
+
+            if (client.Connected)
+            {
+                Console.WriteLine($"Новый клиент {client.RemoteEndPoint} был подключен к серверу");
+            }   
+        }
+    }
+}
